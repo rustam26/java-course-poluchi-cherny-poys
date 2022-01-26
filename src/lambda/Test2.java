16120fa279fd2049e65f80387899b052689ecd3c
rustam26 <rustam.ru26@mail.ru>
2022-01-26 21:26:29 +0300

    Added collection

---
+++ src/lambda/Test2.java
@@ -0,0 +1,17 @@
+package lambda;
+
+public class Test2 {
+
+    static void def (I i){
+        System.out.println(i.abc("privet"));
+    }
+
+    public static void main(String[] args) {
+         def((String str) -> {return  str.length();});
+    }
+
+}
+
+interface I {
+    int abc(String s);
+}
