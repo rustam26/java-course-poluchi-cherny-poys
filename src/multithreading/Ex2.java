2cb7065bc4cf1de6629f9d56e2bccd589ae2a519
rustam26 <rustam.ru26@mail.ru>
2022-01-30 18:17:01 +0300

    Added collection

---
+++ src/multithreading/Ex2.java
@@ -0,0 +1,46 @@
+package multithreading;
+
+public class Ex2  extends Thread{
+
+    public void run(){
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+
+        Ex2 thread1 = new Ex2();
+        thread1.start();
+
+
+        for (int i = 1000; i > 0; i--) {
+            System.out.println(i);
+        }
+
+//        MyThread1 myThread1 = new MyThread1();
+//        MyThread1 myThread3= new MyThread1();
+//        MyThread2 myThread2 = new MyThread2();
+//        myThread1.start();
+//        myThread3.start();
+
+    }
+
+}
+//
+//class MyThread1 extends Thread{
+//    public void run(){
+//        for (int i = 0; i <= 1000; i++) {
+//            System.out.println(i);
+//
+//
+//        }
+//    }
+//}
+//
+//class MyThread2 extends Thread{
+//    public void run(){
+//        for (int i = 1000; i > 0; i--) {
+//            System.out.println(i);
+//        }
+//    }
+//}
---
+++ src/multithreading/Ex3.java
@@ -0,0 +1,46 @@
+package multithreading;
+
+public class Ex3 implements Runnable {
+
+    public void run() {
+        for (int i = 0; i <= 1000; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Ex2();
+        thread1.start();
+
+
+        for (int i = 1000; i > 0; i--) {
+            System.out.println(i);
+        }
+        //            Thread thread1 = new Thread(new MyThread3());
+//            Thread thread2 = new Thread(new MyThread4());
+//
+//            thread1.start();
+//            thread2.start();
+
+    }
+
+}
+
+//class MyThread3 implements Runnable{
+//    public void run(){
+//        for (int i = 0; i <= 1000; i++) {
+//            System.out.println(i);
+//
+//
+//        }
+//    }
+//}
+//
+//class MyThread4 implements Runnable{
+//    public void run(){
+//        for (int i = 1000; i > 0; i--) {
+//            System.out.println(i);
+//        }
+//    }
+//}
