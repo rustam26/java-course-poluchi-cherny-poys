c105ce4187096f1e2698057b5fbd16c19c8db66e
rustam26 <rustam.ru26@mail.ru>
2022-02-04 17:46:32 +0300

    Commit Notebook

--- src/multithreading/VolatileEx.java
+++ src/multithreading/VolatileEx.java
@@ -1,17 +1,13 @@
 package multithreading;
 
 public class VolatileEx  extends Thread{
-
     volatile boolean b = true;
-
     @Override
     public void run() {
         long counter=0;
-
         while (b){
             counter++;
         }
-
         System.out.println("Loop is finished.  counter = " + counter);
     }
 
@@ -20,14 +16,10 @@ public class VolatileEx  extends Thread{
         VolatileEx thread = new VolatileEx();
         thread.start();
         Thread.sleep( 3000);
-
         System.out.println("After 3 seconds it is time to wake up!");
 
         thread.b = false;
         thread.join();
         System.out.println("End of program");
     }
-
-
-
 }
