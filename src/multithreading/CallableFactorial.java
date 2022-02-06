55b86a8b97e94624c20ed6a67123b64846c30c7e
Kosimov Rustam <rustma.ru26@mail.ru>
2022-02-06 20:59:03 +0300

    Commit PC

--- src/multithreading/CallableFactorial.java
+++ src/multithreading/CallableFactorial.java
@@ -11,15 +11,21 @@ public class CallableFactorial {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Factorial2 factorial2 = new Factorial2(5);
         Future<Integer> future = executorService.submit(factorial2);
+
+
+
         try {
+            System.out.println(future.isDone());
+            System.out.println("Xotim poluchit rezultat");
             factorialResult = future.get();
+            System.out.println("Poluchili rezultat");
+            System.out.println(future.isDone());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
 
             System.out.println(e.getCause());
-        }
-        finally {
+        } finally {
             executorService.shutdown();
         }
         System.out.println(factorialResult);
@@ -43,7 +49,8 @@ class Factorial2 implements Callable<Integer> {
 
         int result = 1;
         for (int i = 1; i <= f; i++) {
-            result*=i;
+            result *= i;
+            Thread.sleep(1000);
         }
         return result;
     }
--- src/multithreading/RunnableFactorial.java
+++ src/multithreading/RunnableFactorial.java
@@ -2,6 +2,7 @@ package multithreading;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class RunnableFactorial {
@@ -12,9 +13,13 @@ public class RunnableFactorial {
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Factorial factorial = new Factorial(5);
-        executorService.execute(factorial);
+        //executorService.execute(factorial);
+
+        Future future = executorService.submit(factorial);
+
         executorService.shutdown();
         executorService.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println(future.isDone());
         System.out.println(factorialResult);
     }
 
